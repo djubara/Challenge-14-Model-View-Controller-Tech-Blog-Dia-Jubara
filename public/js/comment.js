7e01f5cd6b116b8d1c67dd9979e0966eadddc208
Dia Jubara <djubara@hotmail.com>
2024-07-23 18:33:56 -0500

    updated the comment and edit-blog js files.

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,17 +1,39 @@
-const newComment = async (event) => {
-    event.preventDefault();
-    const comment = document.querySelector('#comment').value.trim();
-    if (comment) {
-        const response = await axios.post('/api/users/comment', {comment});
-        if (response.status === 200) {
-            document.location.reload();
-            console.log('New comment created');
-        } else {
+let blogId;
+const commentForms = document.querySelectorAll('.new-comment-form');
+
+commentForms.forEach(form => {
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        const blogId = form.dataset.blogid;
+        const comment = form.querySelector('#comment').value.trim();
+
+        if (!comment) {
+            alert('Please enter a comment');
+            return;
+        }
+
+        try {
+            const response = await fetch('/', {
+                method: 'POST',
+                body: JSON.stringify({
+                    blogPost_id: parseInt(blogId),
+                    content: comment,
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.status === 200) {
+                document.location.reload();
+                console.log('New comment created');
+            } else {
+                const data = await response.json();
+                alert('Failed to create new comment');
+            }
+        } catch (error) {
+            console.error('Failed to create new comment', error);
             alert('Failed to create new comment');
         }
-    }
-};
-
-document
-    .querySelector('#commentBtn')
-    .addEventListener('click', newComment);
+    });
+});
--- public/js/edit-blog.js
+++ public/js/edit-blog.js
@@ -1,30 +1,47 @@
-async function editFormHandler(event) {
+
+const editBlogPost = async (event) => {
     event.preventDefault();
-
-    const title = document.querySelector('#title').value.trim();
-    const content = document.querySelector('#content').value.trim();
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ];
-
-    const response = await fetch(`/api/blogs/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-            title,
-            content,
-        }),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
-
-    if (response.ok) {
-        document.location.replace('/');
-    } else {
-        alert('Failed to edit blog');
+    const title = document.querySelector('#postTitle').value.trim();
+    const content = document.querySelector('#postContent').value.trim();
+    const id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
+    if (title && content) {
+        const response = await axios.put(`/api/users/${id}`, { title, content });
+        if (response.status === 200) {
+            document.location.assign('/api/users/dashboard');
+            console.log('New blog post created');
+        } else {
+            alert('Failed to create new blog post');
+        }
     }
+};
+
+const editPageUrl = window.location.href;
+console.log(editPageUrl.split('/'));
+const editPage = editPageUrl.split('/');
+if (!editPage.includes('dashboard')) {
+    document
+        .querySelector('#updatePostBtn')
+        .addEventListener('click', editBlogPost);
+} else {
+    document.querySelector('#updateBtn').style.display = 'none';
 }
 
-document
-    .querySelector('.edit-blog-form')
-    .addEventListener('submit', editFormHandler);
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+
+        const response = await fetch(`/api/users/${id}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            document.location.replace('/api/users/dashboard');
+        } else {
+            alert('Failed to delete blog post');
+        }
+    }
+};
+const allButtons = document.querySelectorAll('#deleteBtn');
+for (const button of allButtons) {
+    button.addEventListener('click', delButtonHandler);
+};
