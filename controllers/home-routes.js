78674ad7d55b2090f762713609bd534a690f0abb
Dia Jubara <djubara@hotmail.com>
2024-07-23 18:24:59 -0500

    updated controllers files.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,11 @@
-// Purpose: To handle the routes for the homepage and login page.
-
 const router = require('express').Router();
 
 
 const { User, BlogPost, Comment } = require('../models');
 
-// login route
+// router.get('/', async (req, res) => {
+//     res.render('home', {loggedIn: req.session.loggedIn});
+// });
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
@@ -15,8 +15,7 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-// homepage route
-
+//get blogposts for homepage
 router.get('/', async (req, res) => {
     try {
         const blogPostData = await BlogPost.findAll({
@@ -40,12 +39,18 @@ router.get('/', async (req, res) => {
             console.log('Blog post content:', blogPost.content);
 
             const post = blogPost.get({ plain: true });
-
+            // post.users = blogPost.User ? blogPost.User.get({ plain: true}) : null;
+            // post.comment = blogPost.Comment ? blogPost.Comment.map((comment) => {
+            //     const com = comment.get({ plain: true });
+            //     com.user = comment.Users ? comment.Users.get({ plain: true}) : null;
+            //     console.log('Plain comment:', com);
+            //     return com;
+            // }) : [];
             console.log('Post:', post);
             return post;
         });
 
-
+        // console.log('comments:', blogPosts[0].comment[0].content);
         console.log('Blog posts with comments:', blogPosts);
         res.render('home', {
             blogPosts,
@@ -56,8 +61,6 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
-
-// TODO move to controller/api/posts-routes.js in a comment controller file.
 let blogId;
 router.post('/', async (req, res) => {
     console.log('req.body', req.body);
@@ -73,8 +76,6 @@ router.post('/', async (req, res) => {
         res.status(400).json(err);
     }
 });
-
-// signup route
 
 router.get('/signup', async (req, res) => {
     res.render('signup');
